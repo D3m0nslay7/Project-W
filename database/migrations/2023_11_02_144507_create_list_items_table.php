4ddb1d9ccf95ddc5e9c970fb7d4c74bdf9f3747f
Umer Mohammed <D3m0nspider@gmail.com>
2023-11-02 08:11:08 -0700

    Added Migrations

---
+++ database/migrations/2023_11_02_144507_create_list_items_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('list_items', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('list_items');
+    }
+};
